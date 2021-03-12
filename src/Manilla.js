478d4dec189fbf255f1f354dbb0d5b22f4d9f66e
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-12 19:01:21 +0200

    nesting components

---
+++ src/Manilla.js
@@ -0,0 +1,15 @@
+import React, {Component } from 'react';
+
+class Manilla extends Component{
+    render(){
+        return(
+            <div className="manilla">
+                <div>Name: Iko</div>
+                <div>Age: 30</div>
+                <div>Belt: Black</div>
+            </div>
+        )
+    }
+}
+
+export default Manilla
