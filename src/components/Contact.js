2819d00ea2a0969704ab817f6f7d4864afc46e09
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-13 11:36:07 +0200

    programmatic redirects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Contacts = () => {
+const Contacts = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
     return(
         <div className="container">
             <h4 className="center">Contacts Page</h4>
